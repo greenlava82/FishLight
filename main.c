47c48c83eb8a232e34f1e592c788a3eda7adb7ab
Dan Tang <dan.tang@microchip.com>
2024-01-19 10:50:07 -0500

    Initial Code seed

---
+++ main.c
@@ -0,0 +1,304 @@
+ /*
+ * MAIN Generated Driver File
+ * 
+ * @file main.c
+ * 
+ * @defgroup main MAIN
+ * 
+ * @brief This is the generated driver implementation file for the MAIN driver.
+ *
+ * @version MAIN Driver Version 1.0.0
+*/
+
+/*
+© [2024] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
+*/
+#include "mcc_generated_files/system/system.h"
+
+/*
+    Main application
+*/
+
+#define PWM_PERIOD          10000ul
+#define BUTTON_ASSERTED     0
+#define BUTTON_IDLE         1
+#define BUTTON_HOLD_TIME    30
+
+// LED Values
+#define LED_MIN_VAL         0.02f
+#define LED_OFF_VAL         0.0f
+#define LED_MAX_VAL         1.0f
+#define LED_DAYLIGHT_VAL    0.85f
+
+// Time Values
+#define TIME_FULL_CYCLE     864000ul
+#define TIME_SUNRISE        18000ul
+#define TIME_SUNSET         18000ul
+#define TIME_DAYLIGHT       432000ul
+#define TIME_EVENING        72000ul
+#define TIME_NIGHT          (TIME_FULL_CYCLE - TIME_SUNRISE - TIME_SUNSET - TIME_DAYLIGHT - TIME_EVENING)
+
+// Fading Math
+#define FADE_SUNRISE_STEP  ((LED_DAYLIGHT_VAL - LED_MIN_VAL) / TIME_SUNRISE)
+#define FADE_SUNSET_STEP   ((LED_DAYLIGHT_VAL - LED_MIN_VAL) / TIME_SUNSET)
+#define FADE_FAST_STEP     0.001f
+
+typedef enum {
+    LED_GREEN = 0,
+    LED_BLUE,
+    LED_RED,
+    LED_ALL
+}LEDColor_t;
+
+typedef enum {
+    BUTTON_RELEASED = 0,
+    BUTTON_NEWPRESS,
+    BUTTON_PRESSED,
+    BUTTON_NEWHOLD,
+    BUTTON_HOLD   
+}BUTTONState_t;
+
+enum SM_State {
+    SM_STARTUP = 0,
+    SM_SUNRISE,
+    SM_DAYTIME,
+    SM_EVENING,
+    SM_SUNSET,
+    SM_NIGHT
+};
+
+enum SM_State State = SM_STARTUP;
+BUTTONState_t buttonState = BUTTON_RELEASED;
+uint8_t tmrCounter = 0;
+bool tmrFlag = false;
+
+void cbTmrCounter(void) {
+    tmrFlag = true;
+}
+
+bool buttonPress = false;
+
+void cbButtonChange(void) {
+    buttonPress = true;
+}
+
+uint8_t setDutyCycle(LEDColor_t channel, float value) {
+    if(LED_MAX_VAL < value) {
+        return 1;
+    }
+    uint16_t regval = (uint16_t)(value*PWM_PERIOD);
+    switch (channel) {
+        case LED_BLUE:
+            PWM1_16BIT_SetSlice1Output1DutyCycleRegister(regval);
+            PWM1_16BIT_LoadBufferRegisters();
+            break;
+        case LED_RED:
+            PWM1_16BIT_SetSlice1Output2DutyCycleRegister(regval);
+            PWM1_16BIT_LoadBufferRegisters();
+            break;
+        case LED_GREEN:
+            PWM2_16BIT_SetSlice1Output1DutyCycleRegister(regval);
+            PWM2_16BIT_LoadBufferRegisters();
+            break;
+        case LED_ALL:
+            PWM1_16BIT_SetSlice1Output1DutyCycleRegister(regval);
+            PWM1_16BIT_SetSlice1Output2DutyCycleRegister(regval);
+            PWM2_16BIT_SetSlice1Output1DutyCycleRegister(regval);
+            PWM1_16BIT_LoadBufferRegisters();
+            PWM2_16BIT_LoadBufferRegisters();
+            break;
+        default:
+            return 1;
+            break;
+    }
+    return 0;
+}
+
+uint32_t dayTimer = 0;
+uint32_t buttonCounter = 0;
+float currentDaylight = LED_OFF_VAL;
+
+int main(void)
+{
+    SYSTEM_Initialize(); 
+    
+    INTERRUPT_GlobalInterruptEnable(); 
+    INTERRUPT_PeripheralInterruptEnable(); 
+    
+    PWM2_16BIT_Enable();
+    PWM2_16BIT_WritePeriodRegister(PWM_PERIOD);
+    
+    PWM1_16BIT_Enable();
+    PWM1_16BIT_WritePeriodRegister(PWM_PERIOD);
+    
+    Timer0_OverflowCallbackRegister(cbTmrCounter);
+    Timer0_Start();
+    
+    IO_RA5_SetInterruptHandler(cbButtonChange);
+    
+    setDutyCycle(LED_ALL, LED_OFF_VAL);
+    
+    while(1)
+    {
+        if (tmrFlag) {
+            tmrFlag = false;
+            
+            // Button Evaluation
+            switch (buttonState) {
+                case BUTTON_RELEASED:
+                    buttonCounter = 0;
+                    if (buttonPress) {
+                        buttonState = BUTTON_NEWPRESS;
+                    }
+                    break;
+                case BUTTON_NEWPRESS:
+                    if (IO_RA5_GetValue() == BUTTON_IDLE) {
+                        buttonState = BUTTON_RELEASED;
+                    }
+                    else {
+                        buttonState = BUTTON_PRESSED;
+                    }
+                    break;
+                case BUTTON_PRESSED:
+                    if (IO_RA5_GetValue() == BUTTON_IDLE) {
+                        buttonState = BUTTON_RELEASED;
+                    }
+                    else if (buttonCounter++ >= BUTTON_HOLD_TIME) {
+                        buttonState = BUTTON_HOLD;
+                    }
+                    break;
+                case BUTTON_NEWHOLD:
+                    if (IO_RA5_GetValue() == BUTTON_IDLE) {
+                        buttonState = BUTTON_RELEASED;
+                    }
+                    else {
+                        buttonState = BUTTON_HOLD;
+                    }
+                    break;
+                case BUTTON_HOLD:
+                    if (IO_RA5_GetValue() == BUTTON_IDLE) {
+                        buttonState = BUTTON_RELEASED;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            buttonPress = false;
+            
+            // LED State Machine
+            switch (State) {
+                case SM_STARTUP:
+                    currentDaylight = LED_MIN_VAL;
+                    setDutyCycle(LED_RED, LED_OFF_VAL);
+                    setDutyCycle(LED_GREEN, LED_OFF_VAL);
+                    setDutyCycle(LED_BLUE, currentDaylight);
+                    if (1) { //buttonState == BUTTON_NEWPRESS) {//(IO_RA5_GetValue() == 0) {
+                        State = SM_SUNRISE;
+                        setDutyCycle(LED_ALL, currentDaylight);
+                    }
+                    break;
+                case SM_SUNRISE:
+                    if ((TIME_SUNRISE < dayTimer) || (buttonState == BUTTON_NEWPRESS)) {
+                        while (currentDaylight < LED_DAYLIGHT_VAL) {
+                            currentDaylight = currentDaylight + FADE_FAST_STEP;
+                            setDutyCycle(LED_ALL, currentDaylight);
+                            __delay_ms(1);
+                        }
+                        State = SM_DAYTIME;
+                        dayTimer = 0;
+                    }
+                    else if (LED_DAYLIGHT_VAL < currentDaylight) {
+                        currentDaylight = LED_DAYLIGHT_VAL;
+                        setDutyCycle(LED_ALL, currentDaylight);
+                    }
+                    else {
+                        currentDaylight += FADE_SUNRISE_STEP;
+                        setDutyCycle(LED_ALL, currentDaylight);
+                    }
+                    break;
+                case SM_DAYTIME:
+                    if ((TIME_DAYLIGHT < dayTimer) || (buttonState == BUTTON_NEWPRESS)) {
+                        if (buttonState == BUTTON_NEWPRESS) {
+                            currentDaylight = LED_DAYLIGHT_VAL - .25;
+                            setDutyCycle(LED_ALL, currentDaylight);
+                        }
+                        State = SM_SUNSET;
+                        dayTimer = 0;
+                    }
+                    break;
+                case SM_SUNSET:
+                    if ((TIME_SUNSET < dayTimer) || (buttonState == BUTTON_NEWPRESS)) {
+                        while (currentDaylight > LED_MIN_VAL) {
+                            currentDaylight = currentDaylight - FADE_FAST_STEP;
+                            setDutyCycle(LED_ALL, currentDaylight);
+                            __delay_ms(1);
+                        }
+                        State = SM_EVENING;
+                        dayTimer = 0;
+                        currentDaylight = LED_MIN_VAL;
+                        setDutyCycle(LED_RED, LED_OFF_VAL);
+                        setDutyCycle(LED_GREEN, currentDaylight);
+                        setDutyCycle(LED_BLUE, currentDaylight);
+                    }
+                    else if (LED_MIN_VAL > currentDaylight) {
+                        currentDaylight = LED_MIN_VAL;
+                        setDutyCycle(LED_ALL, currentDaylight);
+                    }
+                    else {
+                        currentDaylight -= FADE_SUNSET_STEP;
+                        setDutyCycle(LED_ALL, currentDaylight); 
+                    }
+                    break;
+                case SM_EVENING:
+                    if ((TIME_EVENING < dayTimer) || (buttonState == BUTTON_NEWPRESS)){
+                        while (currentDaylight > 0) {
+                            currentDaylight = currentDaylight - FADE_FAST_STEP;
+                            setDutyCycle(LED_BLUE, currentDaylight);
+                            setDutyCycle(LED_GREEN, currentDaylight);
+                            __delay_ms(100);
+                        }
+                        currentDaylight = LED_OFF_VAL;
+                        setDutyCycle(LED_ALL, currentDaylight);
+                        State = SM_NIGHT;
+                        dayTimer = 0;
+                    }
+                    break;
+                case SM_NIGHT: 
+                    if ((TIME_NIGHT < dayTimer) || (buttonState == BUTTON_NEWPRESS)){
+                        while (currentDaylight < LED_MIN_VAL) {
+                            currentDaylight = currentDaylight + FADE_FAST_STEP;
+                            setDutyCycle(LED_ALL, currentDaylight);
+                            __delay_ms(100);
+                        }
+                        State = SM_SUNRISE;
+                        dayTimer = 0;
+                    }
+                    else {
+                        setDutyCycle(LED_ALL, LED_OFF_VAL);
+                    }
+                    break;
+                default:
+                    State = SM_STARTUP;
+                    break;
+            };
+            dayTimer++;
+        }
+    }    
+}
---
+++ mcc_generated_files/system/pins.h
@@ -0,0 +1,208 @@
+/**
+ * Generated Pins header File
+ * 
+ * @file pins.h
+ * 
+ * @defgroup  pinsdriver Pins Driver
+ * 
+ * @brief This is generated driver header for pins. 
+ *        This header file provides APIs for all pins selected in the GUI.
+ *
+ * @version Driver Version  3.0.0
+*/
+
+/*
+© [2024] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
+*/
+
+#ifndef PINS_H
+#define PINS_H
+
+#include <xc.h>
+
+#define INPUT   1
+#define OUTPUT  0
+
+#define HIGH    1
+#define LOW     0
+
+#define ANALOG      1
+#define DIGITAL     0
+
+#define PULL_UP_ENABLED      1
+#define PULL_UP_DISABLED     0
+
+// get/set IO_RA0 aliases
+#define IO_RA0_TRIS                 TRISAbits.TRISA0
+#define IO_RA0_LAT                  LATAbits.LATA0
+#define IO_RA0_PORT                 PORTAbits.RA0
+#define IO_RA0_WPU                  WPUAbits.WPUA0
+#define IO_RA0_OD                   ODCONAbits.ODCA0
+#define IO_RA0_ANS                  ANSELAbits.ANSA0
+#define IO_RA0_SetHigh()            do { LATAbits.LATA0 = 1; } while(0)
+#define IO_RA0_SetLow()             do { LATAbits.LATA0 = 0; } while(0)
+#define IO_RA0_Toggle()             do { LATAbits.LATA0 = ~LATAbits.LATA0; } while(0)
+#define IO_RA0_GetValue()           PORTAbits.RA0
+#define IO_RA0_SetDigitalInput()    do { TRISAbits.TRISA0 = 1; } while(0)
+#define IO_RA0_SetDigitalOutput()   do { TRISAbits.TRISA0 = 0; } while(0)
+#define IO_RA0_SetPullup()          do { WPUAbits.WPUA0 = 1; } while(0)
+#define IO_RA0_ResetPullup()        do { WPUAbits.WPUA0 = 0; } while(0)
+#define IO_RA0_SetPushPull()        do { ODCONAbits.ODCA0 = 0; } while(0)
+#define IO_RA0_SetOpenDrain()       do { ODCONAbits.ODCA0 = 1; } while(0)
+#define IO_RA0_SetAnalogMode()      do { ANSELAbits.ANSA0 = 1; } while(0)
+#define IO_RA0_SetDigitalMode()     do { ANSELAbits.ANSA0 = 0; } while(0)
+// get/set IO_RA1 aliases
+#define IO_RA1_TRIS                 TRISAbits.TRISA1
+#define IO_RA1_LAT                  LATAbits.LATA1
+#define IO_RA1_PORT                 PORTAbits.RA1
+#define IO_RA1_WPU                  WPUAbits.WPUA1
+#define IO_RA1_OD                   ODCONAbits.ODCA1
+#define IO_RA1_ANS                  ANSELAbits.ANSA1
+#define IO_RA1_SetHigh()            do { LATAbits.LATA1 = 1; } while(0)
+#define IO_RA1_SetLow()             do { LATAbits.LATA1 = 0; } while(0)
+#define IO_RA1_Toggle()             do { LATAbits.LATA1 = ~LATAbits.LATA1; } while(0)
+#define IO_RA1_GetValue()           PORTAbits.RA1
+#define IO_RA1_SetDigitalInput()    do { TRISAbits.TRISA1 = 1; } while(0)
+#define IO_RA1_SetDigitalOutput()   do { TRISAbits.TRISA1 = 0; } while(0)
+#define IO_RA1_SetPullup()          do { WPUAbits.WPUA1 = 1; } while(0)
+#define IO_RA1_ResetPullup()        do { WPUAbits.WPUA1 = 0; } while(0)
+#define IO_RA1_SetPushPull()        do { ODCONAbits.ODCA1 = 0; } while(0)
+#define IO_RA1_SetOpenDrain()       do { ODCONAbits.ODCA1 = 1; } while(0)
+#define IO_RA1_SetAnalogMode()      do { ANSELAbits.ANSA1 = 1; } while(0)
+#define IO_RA1_SetDigitalMode()     do { ANSELAbits.ANSA1 = 0; } while(0)
+// get/set IO_RA2 aliases
+#define IO_RA2_TRIS                 TRISAbits.TRISA2
+#define IO_RA2_LAT                  LATAbits.LATA2
+#define IO_RA2_PORT                 PORTAbits.RA2
+#define IO_RA2_WPU                  WPUAbits.WPUA2
+#define IO_RA2_OD                   ODCONAbits.ODCA2
+#define IO_RA2_ANS                  ANSELAbits.ANSA2
+#define IO_RA2_SetHigh()            do { LATAbits.LATA2 = 1; } while(0)
+#define IO_RA2_SetLow()             do { LATAbits.LATA2 = 0; } while(0)
+#define IO_RA2_Toggle()             do { LATAbits.LATA2 = ~LATAbits.LATA2; } while(0)
+#define IO_RA2_GetValue()           PORTAbits.RA2
+#define IO_RA2_SetDigitalInput()    do { TRISAbits.TRISA2 = 1; } while(0)
+#define IO_RA2_SetDigitalOutput()   do { TRISAbits.TRISA2 = 0; } while(0)
+#define IO_RA2_SetPullup()          do { WPUAbits.WPUA2 = 1; } while(0)
+#define IO_RA2_ResetPullup()        do { WPUAbits.WPUA2 = 0; } while(0)
+#define IO_RA2_SetPushPull()        do { ODCONAbits.ODCA2 = 0; } while(0)
+#define IO_RA2_SetOpenDrain()       do { ODCONAbits.ODCA2 = 1; } while(0)
+#define IO_RA2_SetAnalogMode()      do { ANSELAbits.ANSA2 = 1; } while(0)
+#define IO_RA2_SetDigitalMode()     do { ANSELAbits.ANSA2 = 0; } while(0)
+// get/set IO_RA4 aliases
+#define IO_RA4_TRIS                 TRISAbits.TRISA4
+#define IO_RA4_LAT                  LATAbits.LATA4
+#define IO_RA4_PORT                 PORTAbits.RA4
+#define IO_RA4_WPU                  WPUAbits.WPUA4
+#define IO_RA4_OD                   ODCONAbits.ODCA4
+#define IO_RA4_ANS                  ANSELAbits.ANSA4
+#define IO_RA4_SetHigh()            do { LATAbits.LATA4 = 1; } while(0)
+#define IO_RA4_SetLow()             do { LATAbits.LATA4 = 0; } while(0)
+#define IO_RA4_Toggle()             do { LATAbits.LATA4 = ~LATAbits.LATA4; } while(0)
+#define IO_RA4_GetValue()           PORTAbits.RA4
+#define IO_RA4_SetDigitalInput()    do { TRISAbits.TRISA4 = 1; } while(0)
+#define IO_RA4_SetDigitalOutput()   do { TRISAbits.TRISA4 = 0; } while(0)
+#define IO_RA4_SetPullup()          do { WPUAbits.WPUA4 = 1; } while(0)
+#define IO_RA4_ResetPullup()        do { WPUAbits.WPUA4 = 0; } while(0)
+#define IO_RA4_SetPushPull()        do { ODCONAbits.ODCA4 = 0; } while(0)
+#define IO_RA4_SetOpenDrain()       do { ODCONAbits.ODCA4 = 1; } while(0)
+#define IO_RA4_SetAnalogMode()      do { ANSELAbits.ANSA4 = 1; } while(0)
+#define IO_RA4_SetDigitalMode()     do { ANSELAbits.ANSA4 = 0; } while(0)
+// get/set IO_RA5 aliases
+#define IO_RA5_TRIS                 TRISAbits.TRISA5
+#define IO_RA5_LAT                  LATAbits.LATA5
+#define IO_RA5_PORT                 PORTAbits.RA5
+#define IO_RA5_WPU                  WPUAbits.WPUA5
+#define IO_RA5_OD                   ODCONAbits.ODCA5
+#define IO_RA5_ANS                  ANSELAbits.ANSA5
+#define IO_RA5_SetHigh()            do { LATAbits.LATA5 = 1; } while(0)
+#define IO_RA5_SetLow()             do { LATAbits.LATA5 = 0; } while(0)
+#define IO_RA5_Toggle()             do { LATAbits.LATA5 = ~LATAbits.LATA5; } while(0)
+#define IO_RA5_GetValue()           PORTAbits.RA5
+#define IO_RA5_SetDigitalInput()    do { TRISAbits.TRISA5 = 1; } while(0)
+#define IO_RA5_SetDigitalOutput()   do { TRISAbits.TRISA5 = 0; } while(0)
+#define IO_RA5_SetPullup()          do { WPUAbits.WPUA5 = 1; } while(0)
+#define IO_RA5_ResetPullup()        do { WPUAbits.WPUA5 = 0; } while(0)
+#define IO_RA5_SetPushPull()        do { ODCONAbits.ODCA5 = 0; } while(0)
+#define IO_RA5_SetOpenDrain()       do { ODCONAbits.ODCA5 = 1; } while(0)
+#define IO_RA5_SetAnalogMode()      do { ANSELAbits.ANSA5 = 1; } while(0)
+#define IO_RA5_SetDigitalMode()     do { ANSELAbits.ANSA5 = 0; } while(0)
+#define RA5_SetInterruptHandler  IO_RA5_SetInterruptHandler
+/**
+ * @ingroup  pinsdriver
+ * @brief GPIO and peripheral I/O initialization
+ * @param none
+ * @return none
+ */
+void PIN_MANAGER_Initialize (void);
+
+/**
+ * @ingroup  pinsdriver
+ * @brief Interrupt on Change Handling routine
+ * @param none
+ * @return none
+ */
+void PIN_MANAGER_IOC(void);
+
+/**
+ * @ingroup  pinsdriver
+ * @brief Interrupt on Change Handler for the IO_RA5 pin functionality
+ * @param none
+ * @return none
+ */
+void IO_RA5_ISR(void);
+
+/**
+ * @ingroup  pinsdriver
+ * @brief Interrupt Handler Setter for IO_RA5 pin interrupt-on-change functionality.
+ *        Allows selecting an interrupt handler for IO_RA5 at application runtime.
+ * @pre Pins intializer called
+ * @param InterruptHandler function pointer.
+ * @return none
+ */
+void IO_RA5_SetInterruptHandler(void (* InterruptHandler)(void));
+
+/**
+ * @ingroup  pinsdriver
+ * @brief Dynamic Interrupt Handler for IO_RA5 pin.
+ *        This is a dynamic interrupt handler to be used together with the IO_RA5_SetInterruptHandler() method.
+ *        This handler is called every time the IO_RA5 ISR is executed and allows any function to be registered at runtime.
+ * @pre Pins intializer called
+ * @param none
+ * @return none
+ */
+extern void (*IO_RA5_InterruptHandler)(void);
+
+/**
+ * @ingroup  pinsdriver
+ * @brief Default Interrupt Handler for IO_RA5 pin. 
+ *        This is a predefined interrupt handler to be used together with the IO_RA5_SetInterruptHandler() method.
+ *        This handler is called every time the IO_RA5 ISR is executed. 
+ * @pre Pins intializer called
+ * @param none
+ * @return none
+ */
+void IO_RA5_DefaultInterruptHandler(void);
+
+
+#endif // PINS_H
+/**
+ End of File
+*/
